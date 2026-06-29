c7084330a84b00226112adc65a61e0d71e67d4e1
Azimwafi <azimwafi67@gmail.com>
2026-06-29 15:18:37 +0800

    Add additional SauceDemo login tests

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -1,9 +1,14 @@
 package ui;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+
+import java.time.Duration;
 
 public class LoginTest extends BaseTest {
 
@@ -25,5 +30,32 @@ public class LoginTest extends BaseTest {
         loginPage.login("standard_user", "wrong_password");
 
         Assert.assertTrue(loginPage.getErrorMessage().contains("Username and password do not match"));
+    }
+
+    @Test
+    public void lockedOutUserCannotLogin() {
+        driver.get("https://www.saucedemo.com/");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("locked_out_user", "secret_sauce");
+
+        Assert.assertTrue(loginPage.getErrorMessage().contains("locked out"));
+    }
+
+    @Test
+    public void userCanLogoutSuccessfully() {
+        driver.get("https://www.saucedemo.com/");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("logout_sidebar_link"))).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-button")));
+Assert.assertTrue(driver.getCurrentUrl().contains("saucedemo.com"));
     }
 }
