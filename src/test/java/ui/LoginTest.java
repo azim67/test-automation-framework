386d4602e6c81450d3d1ce0216b42a803496d291
Azimwafi <azimwafi67@gmail.com>
2026-06-29 15:24:12 +0800

    Remove flaky logout test

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -1,14 +1,10 @@
 package ui;
 
 import base.BaseTest;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
-
-import java.time.Duration;
 
 public class LoginTest extends BaseTest {
 
@@ -42,20 +38,5 @@ public class LoginTest extends BaseTest {
         Assert.assertTrue(loginPage.getErrorMessage().contains("locked out"));
     }
 
-    @Test
-    public void userCanLogoutSuccessfully() {
-        driver.get("https://www.saucedemo.com/");
-
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("standard_user", "secret_sauce");
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        driver.findElement(By.id("react-burger-menu-btn")).click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("logout_sidebar_link"))).click();
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-button")));
-Assert.assertTrue(driver.getCurrentUrl().contains("saucedemo.com"));
-    }
+  
 }
